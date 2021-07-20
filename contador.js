6ffa59253ba7091466bb55d414690561eaad79a4
Juan Campos <juancampos.bsi@outlook.com>
2021-07-19 22:55:12 -0300

    first commit

---
+++ contador.js
@@ -0,0 +1,33 @@
+window.addEventListener("load", () =>{
+//tempo em segundos que queremos
+let sec = 360;
+
+const countDIv = document.getElementById("timer");
+
+const secpass = () => {
+  let min = Math.floor(sec/60);
+  let segundosRestantes = sec % 60;
+
+  if(segundosRestantes < 10){
+    segundosRestantes = "0" + segundosRestantes;
+  }
+  if(min < 10){
+    min = "0" + min;
+  }
+  //vai gerar o formato de 02:54
+  countDIv.innerHTML = min +":" + segundosRestantes;
+
+  //3:7
+  //03:11
+
+  //Condição final
+  if (sec > 0){
+    sec = sec -1;
+  } 
+  else {
+    countDiv.innerHTML = "Acabou";
+  }
+};
+
+const countDown = setInterval(() => secpass(), 1000);
+});
